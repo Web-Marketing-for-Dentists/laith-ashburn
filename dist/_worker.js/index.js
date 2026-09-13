041e492768e1edea84df027502ef53a81015f0af
whitney-wm4d <whitney@wm4d.com>
2026-09-13 14:36:13 -0400

    claude fixes

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CwtSofnh.mjs';
-import { manifest } from './manifest_DUDvoNIR.mjs';
+import { manifest } from './manifest_L2XwZdWW.mjs';
 
 const serverIslandMap = new Map();;
 
